61cba098cee2eed99985e4033973a1f2576a760e
Casey Henderson <caseyhenderson13@gmail.com>
2022-12-05 20:15:25 +0000

    FP-001: Initial project commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to functional-assignment
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/functional_assignment/core_test.clj
@@ -0,0 +1,7 @@
+(ns functional-assignment.core-test
+  (:require [clojure.test :refer :all]
+            [functional-assignment.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
